ad2423116d77c179287a3d5f4c1713d0190d6e1f
Abdelrahman Abdellatif <36771724+AbderrhmanAbdellatif@users.noreply.github.com>
2022-06-11 00:37:02 +0300

    Finish

--- ProgrammersBlog.Mvc/Controllers/HomeController.cs
+++ ProgrammersBlog.Mvc/Controllers/HomeController.cs
@@ -12,21 +12,26 @@ using ProgrammersBlog.Shared.Utilities.Helpers.Abstract;
 
 namespace ProgrammersBlog.Mvc.Controllers
 {
+    [Route("/")]
     public class HomeController : Controller
     {
         private readonly IArticleService _articleService;
         private readonly AboutUsPageInfo _aboutUsPageInfo;
         private readonly IMailService _mailService;
         private readonly IToastNotification _toastNotification;
-        private readonly IWritableOptions<AboutUsPageInfo> _aboutUsPageInfoWrite;
-        public HomeController(IArticleService articleService, IOptionsSnapshot<AboutUsPageInfo> aboutUsPageInfo, IMailService mailService, IToastNotification toastNotification, IWritableOptions<AboutUsPageInfo> aboutUsPageInfoWrite)
+        private readonly IWritableOptions<AboutUsPageInfo> _aboutUsPageInfoWriter;
+
+        public HomeController(IArticleService articleService, IOptionsSnapshot<AboutUsPageInfo> aboutUsPageInfo, IMailService mailService, IToastNotification toastNotification, IWritableOptions<AboutUsPageInfo> aboutUsPageInfoWriter)
         {
             _articleService = articleService;
             _mailService = mailService;
             _toastNotification = toastNotification;
+            _aboutUsPageInfoWriter = aboutUsPageInfoWriter;
             _aboutUsPageInfo = aboutUsPageInfo.Value;
-            _aboutUsPageInfoWrite = aboutUsPageInfoWrite;
         }
+        [Route("index")]
+        [Route("anasayfa")]
+        [Route("")]
         [HttpGet]
         public async Task<IActionResult> Index(int? categoryId, int currentPage = 1, int pageSize = 5, bool isAscending = false)
         {
@@ -35,17 +40,20 @@ namespace ProgrammersBlog.Mvc.Controllers
                 : _articleService.GetAllByPagingAsync(categoryId.Value, currentPage, pageSize, isAscending));
             return View(articlesResult.Data);
         }
+        [Route("hakkimizda")]
+        [Route("hakkinda")]
         [HttpGet]
         public IActionResult About()
         {
-          
             return View(_aboutUsPageInfo);
         }
+        [Route("iletisim")]
         [HttpGet]
         public IActionResult Contact()
         {
             return View();
         }
+        [Route("iletisim")]
         [HttpPost]
         public IActionResult Contact(EmailSendDto emailSendDto)
         {
